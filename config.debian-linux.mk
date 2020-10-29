c6fe69075aa9979113064a3c89492b05539c897b
eindiran <eindiran@uchicago.edu>
2020-10-28 17:14:19 -0700

    Add new distro-specific config.mk files

---
+++ config.debian-linux.mk
@@ -0,0 +1,30 @@
+# st version
+VERSION = 0.8.2
+
+# Paths
+PREFIX ?= /usr/local
+MANPREFIX = $(PREFIX)/share/man
+
+# Include X11 in Ubuntu
+X11INC = /usr/include/X11R6
+X11LIB = /usr/lib/X11R6
+
+PKG_CONFIG = pkg-config
+
+# Includes and libs
+INCS = -I$(X11INC) \
+       `$(PKG_CONFIG) --cflags fontconfig` \
+       `$(PKG_CONFIG) --cflags freetype2` \
+       `$(PKG_CONFIG) --cflags harfbuzz`
+LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft -lXrender\
+       `$(PKG_CONFIG) --libs fontconfig` \
+       `$(PKG_CONFIG) --libs freetype2` \
+       `$(PKG_CONFIG) --libs harfbuzz`
+
+# Flags
+STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
+STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
+STLDFLAGS = $(LIBS) $(LDFLAGS)
+
+# Compiler and linker
+CC = c99
---
+++ config.openbsd.mk
@@ -0,0 +1,29 @@
+# st version
+VERSION = 0.8.2
+
+# Paths
+PREFIX ?= /usr/local
+MANPREFIX = $(PREFIX)/share/man
+
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+
+PKG_CONFIG = pkg-config
+
+# Includes and libs
+INCS = -I$(X11INC) \
+       `$(PKG_CONFIG) --cflags fontconfig` \
+       `$(PKG_CONFIG) --cflags freetype2`  \
+       `$(PKG_CONFIG) --cflags harfbuzz`
+LIBS = -L$(X11LIB) -lm -lX11 -lutil -lXft  \
+       `$(PKG_CONFIG) --libs fontconfig`   \
+       `$(PKG_CONFIG) --libs freetype2`    \
+       `$(PKG_CONFIG) --libs harfbuzz`
+
+# Flags
+CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 -D_BSD_SOURCE
+STCFLAGS = $(INCS) $(CPPFLAGS) $(CFLAGS)
+STLDFLAGS = $(LIBS) $(LDFLAGS)
+
+# Compiler and linker
+CC = c99
